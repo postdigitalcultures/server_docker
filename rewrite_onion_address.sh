605cb77d5094c5a95eda9d261f34c8840f93bfc5
SimonXIX <ad7588@coventry.ac.uk>
2023-09-06 14:05:15 +0100

    adjusted onion service script

--- rewrite_onion_address.sh
+++ rewrite_onion_address.sh
@@ -6,16 +6,31 @@
 # @author: Simon Bowie <ad7588@coventry.ac.uk>
 # @purpose: Rewrites the onion addresses in our WordPress theme files to the correct onion address
 
-# define variables
+# VARIABLES
 WORDPRESS_THEME_DIRECTORY='/home/cpc_admin/docker/wordpress/wp-content/themes/postdigital-cultures'
 ONION_ADDRESS=`docker exec tor cat /var/lib/tor/onion_services/onion-nginx/hostname`
 #ONION_ADDRESS='testing.onion'
+SQL_QUERY='UPDATE wordpress.wp_domain_mapping SET domain="'$ONION_ADDRESS'" WHERE blog_id=1;'
 
+# get secure variables from .env file
+eval "$(
+  cat .env | awk '!/^\s*#/' | awk '!/^\s*$/' | while IFS='' read -r line; do
+    key=$(echo "$line" | cut -d '=' -f 1)
+    value=$(echo "$line" | cut -d '=' -f 2-)
+    echo "export $key=\"$value\""
+  done
+)"
+
+# MAIN PROGRAM
 # find the Onion-Location header and replace ONION_ADDRESS (or the .onion address) with the new onion address
 # development version for macOS which implements 'sed' differently
 #find ${WORDPRESS_THEME_DIRECTORY} \( -name "*.php" \) -exec gsed -Ei "s,[a-zA-Z0-9]+\.onion,${ONION_ADDRESS},gI" {} \;
 
 # production version for Linux
 find ${WORDPRESS_THEME_DIRECTORY} \( -name "*.php" \) -exec sed -Ei "s,[a-zA-Z0-9]+\.onion,${ONION_ADDRESS},gI" {} \;
+
+# replace the value on the wp_domain_mapping table with appropriate ONION_ADDRESS
+docker exec mariadb mysql -u $MARIADB_USER -p$MARIADB_PASSWORD -e "$SQL_QUERY"
+
 
 exit
